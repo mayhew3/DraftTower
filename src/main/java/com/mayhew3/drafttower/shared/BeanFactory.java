28cea1c214826cda6dc839de1275f05e00e62d03
Gus Prevas <kprevas@gmail.com>
2013-01-26 14:09:48 -0500

    refactor client-side draft clock controls to use command object

--- src/main/java/com/mayhew3/drafttower/shared/BeanFactory.java
+++ src/main/java/com/mayhew3/drafttower/shared/BeanFactory.java
@@ -1,13 +1,12 @@
 package com.mayhew3.drafttower.shared;
 
-import com.google.inject.Singleton;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
 
 /**
  * Factory interface for creating {@link AutoBean}s.
  */
-@Singleton
 public interface BeanFactory extends AutoBeanFactory {
   AutoBean<DraftStatus> createDraftStatus();
+  AutoBean<DraftCommand> createDraftCommand();
 }
