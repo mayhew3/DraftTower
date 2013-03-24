883a3fbeac0b4069f81ebe59c53a917444049b8a
Gus Prevas <kprevas@gmail.com>
2013-03-24 14:53:41 -0400

    don't set robot mode when auto-pick is from queue

--- src/test/java/com/mayhew3/drafttower/server/DraftControllerTest.java
+++ src/test/java/com/mayhew3/drafttower/server/DraftControllerTest.java
@@ -5,8 +5,10 @@ import com.google.common.collect.Lists;
 import com.google.guiceberry.junit4.GuiceBerryRule;
 import com.google.inject.Inject;
 import com.mayhew3.drafttower.server.BindingAnnotations.Keepers;
+import com.mayhew3.drafttower.server.BindingAnnotations.Queues;
 import com.mayhew3.drafttower.shared.BeanFactory;
 import com.mayhew3.drafttower.shared.DraftStatus;
+import com.mayhew3.drafttower.shared.QueueEntry;
 import junit.framework.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,10 +24,12 @@ public class DraftControllerTest {
   @Inject private DraftController draftController;
   @Inject private DraftStatus draftStatus;
   @Inject @Keepers private ListMultimap<Integer, Integer> keepers;
+  @Inject @Queues private ListMultimap<Integer, QueueEntry> queues;
   @Inject private BeanFactory beanFactory;
 
   @Test
   public void testBackOutLastPickSkipsKeepers() throws Exception {
+    reset();
     draftStatus.setPicks(Lists.newArrayList(
         beanFactory.createDraftPick().as(),
         beanFactory.createDraftPick().as(),
@@ -36,5 +40,30 @@ public class DraftControllerTest {
     draftController.backOutLastPick();
     Assert.assertEquals(3, draftStatus.getCurrentTeam());
     Assert.assertEquals(2, draftStatus.getPicks().size());
+  }
+
+  @Test
+  public void testAutoPickSetsRobotMode() throws Exception {
+    reset();
+    draftController.timerExpired();
+    Assert.assertTrue(draftStatus.getRobotTeams().contains(1));
+  }
+
+  @Test
+  public void testAutoPickFromQueueDoesNotSetRobotMode() throws Exception {
+    reset();
+    QueueEntry queueEntry = beanFactory.createQueueEntry().as();
+    queueEntry.setPlayerId(0);
+    queues.put(1, queueEntry);
+    draftController.timerExpired();
+    Assert.assertFalse(draftStatus.getRobotTeams().contains(1));
+  }
+
+  private void reset() {
+    draftStatus.getPicks().clear();
+    draftStatus.setCurrentTeam(1);
+    draftStatus.getRobotTeams().clear();
+    keepers.clear();
+    queues.clear();
   }
 }
