9b500bf1fb292db580124d9a64655244cdb220fa
Mayhew Seavey <scorpy@gmail.com>
2016-03-14 22:46:25 -0700

    Expand updater to add and split players added this year. No name-change validation for these. Also add handler for players with multi-position in the name e.g. C-SS or 1B-C.

--- src/main/java/com/mayhew3/drafttower/server/database/ExistingPlayerUpdater.java
+++ src/main/java/com/mayhew3/drafttower/server/database/ExistingPlayerUpdater.java
@@ -3,11 +3,9 @@ package com.mayhew3.drafttower.server.database;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.mayhew3.drafttower.server.database.dataobject.CbsID;
 import com.mayhew3.drafttower.server.database.dataobject.FieldValue;
 import com.mayhew3.drafttower.server.database.dataobject.Player;
-import com.mayhew3.drafttower.server.database.dataobject.TmpProjectionBatter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,7 +24,20 @@ public class ExistingPlayerUpdater {
   }
 
   public void updateDatabase() throws SQLException {
+    insertNewPlayers();
     splitNames();
+  }
+
+  private void insertNewPlayers() throws SQLException {
+    String sql = "INSERT INTO Players (PlayerString, CBS_ID, CreateTime, UpdateTime) " +
+        "SELECT cbs.PlayerString, cbs.CBS_ID, NOW(), NOW() " +
+        "FROM cbsids cbs " +
+        "LEFT OUTER JOIN Players p " +
+        "  ON cbs.CBS_ID = p.CBS_ID " +
+        "WHERE p.CBS_ID IS NULL;";
+    connection.prepareAndExecuteStatementUpdate(sql);
+
+    // todo: print and validate number of rows inserted.
   }
 
   private void splitNames() throws SQLException {
@@ -95,7 +106,22 @@ public class ExistingPlayerUpdater {
     player.mlbTeam.changeValue(changedPlayer.MLBTeam);
     player.position.changeValue(changedPlayer.Position);
 
-    logger.log(Level.INFO, "Updating '" + oldPlayerString + "' (" + player + ") to '" + newPlayerString + "'");
+    // first-time split, don't need to validate it's the same player.
+    if (!newPlayerString.equals(oldPlayerString)) {
+      validateChangesToExistingPlayer(player, id, newPlayerString, oldPlayerString);
+    }
+
+    player.playerString.changeValue(newPlayerString);
+
+    if (player.hasChanged()) {
+      player.updateTime.changeValue(new Date());
+    }
+
+    player.commit(connection);
+  }
+
+  private void validateChangesToExistingPlayer(Player player, Integer id, String newPlayerString, String oldPlayerString) throws SQLException, FailedPlayer {
+    logger.log(Level.INFO, "Validating change of '" + oldPlayerString + "' (" + player + ") to '" + newPlayerString + "'");
 
     List<FieldValue> changedFields = player.getChangedFields();
 
@@ -118,14 +144,6 @@ public class ExistingPlayerUpdater {
         throw new FailedPlayer(id, "Too many differences for " + oldPlayerString + " -> " + newPlayerString);
       }
     }
-
-    player.playerString.changeValue(newPlayerString);
-
-    if (player.hasChanged()) {
-      player.updateTime.changeValue(new Date());
-    }
-
-    player.commit(connection);
   }
 
   private static boolean areCompatible(String position1, String position2) {
@@ -142,6 +160,21 @@ public class ExistingPlayerUpdater {
   }
 
   private static PlayerInfo parseFromString(int id, String playerString, String previousLastName) throws FailedPlayer {
+    List<String> validPositions = Lists.newArrayList(
+        "C",
+        "1B",
+        "2B",
+        "3B",
+        "SS",
+        "OF",
+        "LF",
+        "CF",
+        "RF",
+        "SP",
+        "RP",
+        "P"
+    );
+
     PlayerInfo playerInfo = new PlayerInfo();
 
     List<String> parts = Lists.newArrayList(playerString.split(" "));
@@ -162,55 +195,17 @@ public class ExistingPlayerUpdater {
       throw new FailedPlayer(id, "Incorrect team name '" + playerInfo.MLBTeam + "', from player string '" + playerString + "'");
     }
 
-    if (playerInfo.Position.length() < 1 || playerInfo.Position.length() > 2) {
-      throw new FailedPlayer(id, "Incorrect position '" + playerInfo.Position + "', from player string '" + playerString + "'");
+    if (!validPositions.contains(playerInfo.Position)) {
+      String[] multiPositions = playerInfo.Position.split("-");
+      if (multiPositions.length < 2 || !validPositions.contains(multiPositions[0])) {
+        throw new FailedPlayer(id, "Incorrect position '" + playerInfo.Position + "', from player string '" + playerString + "'");
+      } else {
+        playerInfo.Position = multiPositions[0];
+      }
     }
 
     if (parts.size() > 2) {
-
-      List<String> potentialLastNames = new ArrayList<>();
-      List<String> potentialFirstNames = new ArrayList<>();
-
-      Integer maxFirstName = parts.size() - 1;
-      for (int firstNameSize = 1; firstNameSize <= maxFirstName; firstNameSize++) {
-        List<String> lastNameStrings = parts.subList(firstNameSize, parts.size());
-
-        Joiner joiner = Joiner.on(" ");
-        String potentialLastName = joiner.join(lastNameStrings);
-        List<String> firstNameStrings = parts.subList(0, firstNameSize);
-        String potentialFirstName = joiner.join(firstNameStrings);
-
-        if (Objects.equals(previousLastName, potentialLastName)) {
-
-          playerInfo.firstName = potentialFirstName;
-          playerInfo.lastName = potentialLastName;
-
-          return playerInfo;
-        }
-
-        potentialLastNames.add(potentialLastName);
-        potentialFirstNames.add(potentialFirstName);
-      }
-
-      List<String> displayOptions = new ArrayList<>();
-      for (int i = 0; i < potentialLastNames.size(); i++) {
-        displayOptions.add(i+1 + ") '" + potentialLastNames.get(i) + "'");
-      }
-      Joiner commaJoiner = Joiner.on(", ");
-      String selectedOption = InputGetter.grabInput("'" + playerString + "'  Potential last names: " + commaJoiner.join(displayOptions) + ", 0) None of these. ");
-      Integer selectedIndex = Integer.valueOf(selectedOption);
-
-      if (selectedIndex == null || selectedIndex == 0) {
-        throw new FailedPlayer(id, "Full name had more than three parts, and no combination matched existing LastName field.");
-      } else {
-        String selectedLastName = potentialLastNames.get(selectedIndex - 1);
-        String selectedFirstName = potentialFirstNames.get(selectedIndex - 1);
-
-        playerInfo.lastName = selectedLastName;
-        playerInfo.firstName = selectedFirstName;
-
-        return playerInfo;
-      }
+      return parseExtraNames(id, playerString, previousLastName, playerInfo, parts);
     } else if (parts.size() < 2) {
       throw new FailedPlayer(id, "Found fewer than two parts to full name.");
     } else {
@@ -221,6 +216,53 @@ public class ExistingPlayerUpdater {
     }
   }
 
+  private static PlayerInfo parseExtraNames(int id, String playerString, String previousLastName, PlayerInfo playerInfo, List<String> parts) throws FailedPlayer {
+    // todo: check DB for existing multi-part names on other players
+
+    List<String> potentialLastNames = new ArrayList<>();
+    List<String> potentialFirstNames = new ArrayList<>();
+
+    Integer maxFirstName = parts.size() - 1;
+    for (int firstNameSize = 1; firstNameSize <= maxFirstName; firstNameSize++) {
+      List<String> lastNameStrings = parts.subList(firstNameSize, parts.size());
+
+      Joiner joiner = Joiner.on(" ");
+      String potentialLastName = joiner.join(lastNameStrings);
+      List<String> firstNameStrings = parts.subList(0, firstNameSize);
+      String potentialFirstName = joiner.join(firstNameStrings);
+
+      if (Objects.equals(previousLastName, potentialLastName)) {
+
+        playerInfo.firstName = potentialFirstName;
+        playerInfo.lastName = potentialLastName;
+
+        return playerInfo;
+      }
+
+      potentialLastNames.add(potentialLastName);
+      potentialFirstNames.add(potentialFirstName);
+    }
+
+    List<String> displayOptions = new ArrayList<>();
+    for (int i = 0; i < potentialLastNames.size(); i++) {
+      displayOptions.add(i+1 + ") '" + potentialLastNames.get(i) + "'");
+    }
+    Joiner commaJoiner = Joiner.on(", ");
+    String selectedOption = InputGetter.grabInput("'" + playerString + "'  Potential last names: " + commaJoiner.join(displayOptions) + ", 0) None of these. ");
+    Integer selectedIndex = Integer.valueOf(selectedOption);
+
+    if (selectedIndex == null || selectedIndex == 0) {
+      throw new FailedPlayer(id, "Full name had more than three parts, and no combination matched existing LastName field.");
+    } else {
+      String selectedLastName = potentialLastNames.get(selectedIndex - 1);
+      String selectedFirstName = potentialFirstNames.get(selectedIndex - 1);
+
+      playerInfo.lastName = selectedLastName;
+      playerInfo.firstName = selectedFirstName;
+
+      return playerInfo;
+    }
+  }
 
 
 }
