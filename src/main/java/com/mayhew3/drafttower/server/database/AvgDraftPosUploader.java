09c8f4b7f69b72a906b2ff74eabb86daeaaa7804
Mayhew Seavey <scorpy@gmail.com>
2016-03-15 18:54:12 -0700

    Add AvgDraftPositions

---
+++ src/main/java/com/mayhew3/drafttower/server/database/AvgDraftPosUploader.java
@@ -0,0 +1,35 @@
+package com.mayhew3.drafttower.server.database;
+
+import com.google.common.collect.Lists;
+import com.mayhew3.drafttower.server.database.dataobject.TmpDraftAveragesFactory;
+import com.mayhew3.drafttower.server.database.dataobject.TmpEligibilityFactory;
+import org.joda.time.LocalDate;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+public class AvgDraftPosUploader {
+  private SQLConnection connection;
+  private LocalDate statsDate;
+
+  public AvgDraftPosUploader(SQLConnection connection, LocalDate statsDate) {
+    this.connection = connection;
+    this.statsDate = statsDate;
+  }
+
+  public void updateDatabase() throws IOException, SQLException {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMdd");
+
+    String pathname = "database/" + statsDate.getYear() + "/avgDraft" + simpleDateFormat.format(statsDate.toDate()) + ".csv";
+
+    File file = new File(pathname);
+    FileReader fileReader = new FileReader(file);
+
+    PlayerListParser playerListParser = new PlayerListParser(fileReader, new TmpDraftAveragesFactory(), statsDate, Lists.newArrayList("Rank", "Avg Pick", "HI/LO", "% Drafted"));
+    playerListParser.uploadPlayersToDatabase(connection);
+  }
+
+}
---
+++ src/main/java/com/mayhew3/drafttower/server/database/dataobject/TmpDraftAverages.java
@@ -0,0 +1,15 @@
+package com.mayhew3.drafttower.server.database.dataobject;
+
+public class TmpDraftAverages extends TmpStatTable {
+
+  public FieldValueBigDecimal avg_pick = registerBigDecimalField("Avg Pick");
+  public FieldValueBigDecimal percent_drafted = registerBigDecimalField("% Drafted");
+  public FieldValueString hi_lo = registerStringField("HI/LO");
+  public FieldValueInteger playerID = registerIntegerField("PlayerID");
+  public FieldValueInteger rank = registerIntegerField("Rank");
+
+  @Override
+  protected String getTableName() {
+    return "cbs_draftaverages";
+  }
+}
