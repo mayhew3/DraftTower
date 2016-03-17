7a9acff39890177597bb782f86a2b4ffa1a921ac
Mayhew Seavey <scorpy@gmail.com>
2016-03-16 21:42:10 -0700

    Update keepers utility with new connection object.

--- src/main/java/com/mayhew3/drafttower/server/database/PopulateKeepers.java
+++ src/main/java/com/mayhew3/drafttower/server/database/PopulateKeepers.java
@@ -1,71 +1,54 @@
 package com.mayhew3.drafttower.server.database;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class PopulateKeepers extends DatabaseUtility {
+public class PopulateKeepers {
+
+  private SQLConnection connection;
+
+  public PopulateKeepers(SQLConnection connection) {
+    this.connection = connection;
+  }
 
   @SuppressWarnings("SpellCheckingInspection")
-  public static void main(String[] args) {
-    initConnection();
-
+  public void updateDatabase() throws SQLException {
     System.out.println("Adding keepers.");
 
     removeExistingKeepers();
 
-    addKeeper("gus", "Goldschmidt, Paul");
-    addKeeper("gus", "Rizzo, Anthony");
-    addKeeper("gus", "Rendon, Anthony");
-    addKeeper("mayhews", "McCutchen, Andrew");
-    addKeeper("mayhews", "Hernandez, Felix");
-    addKeeper("laura", "Trout, Mike");
-    addKeeper("laura", "Donaldson, Josh");
-    addKeeper("alcides", "Cabrera, Miguel");
-    addKeeper("lakshmi", "Kershaw, Clayton");
-    addKeeper("lakshmi", "Altuve, Jose");
-    addKeeper("lakshmi", "Brantley, Michael");
-    addKeeper("scott", "Bautista, Jose");
-    addKeeper("scott", "Encarnacion, Edwin");
-    addKeeper("scott", "Price, David");
-    addKeeper("eric", "Stanton, Giancarlo");
-    addKeeper("eric", "Abreu, Jose");
-    addKeeper("eric", "Kluber, Corey");
+    addKeeper("gus", "Harper", "Bryce");
+    addKeeper("gus", "Rizzo", "Anthony");
+    addKeeper("gus", "Betts", "Mookie");
   }
 
 
-  private static void addKeeper(String ownerName, String playerString) {
-    String[] split = playerString.split(", ");
-    String lastName = split[0];
-    String firstName = split[1];
-
-    String sql = "INSERT INTO Keepers (TeamID, PlayerID) " +
-        "VALUES (" + getTeamID(ownerName) + ", " + getPlayerIDFromFirstAndLastName(firstName, lastName) + ")";
-
-    executeUpdate(sql);
+  private void addKeeper(String ownerName, String lastName, String firstName) throws SQLException {
+    connection.prepareAndExecuteStatementUpdate("INSERT INTO Keepers (TeamID, PlayerID) VALUES (?, ?)",
+        getTeamID(ownerName), getPlayerIDFromFirstAndLastName(firstName, lastName));
   }
 
-  private static int getTeamID(String ownerName) {
-    ResultSet resultSet = executeQuery("SELECT ID FROM teams WHERE UserID = '" + ownerName + "'");
+  private Integer getTeamID(String ownerName) throws SQLException {
+    ResultSet resultSet = connection.prepareAndExecuteStatementFetch("SELECT ID FROM teams WHERE UserID = ?", ownerName);
+    resultSet.next();
 
-    if (hasMoreElements(resultSet)) {
-      return getInt(resultSet, "ID");
+    return resultSet.getInt("ID");
+  }
+
+  private int getPlayerIDFromFirstAndLastName(String firstName, String lastName) throws SQLException {
+    ResultSet resultSet = connection.prepareAndExecuteStatementFetch("SELECT ID FROM Players WHERE FirstName = ? AND LastName = ?", firstName, lastName);
+    if (resultSet.next()) {
+      int playerID = resultSet.getInt("ID");
+      if (resultSet.next()) {
+        throw new RuntimeException("Found multiple matches for player " + firstName + " " + lastName);
+      }
+      return playerID;
     } else {
-      throw new IllegalStateException("Couldn't find team with UserID " + ownerName);
-    }
-  }
-
-  private static int getPlayerIDFromFirstAndLastName(String firstName, String lastName) {
-    ResultSet resultSet = prepareAndExecuteStatementFetch("SELECT ID FROM Players WHERE FirstName = ? AND LastName = ?", firstName, lastName);
-    if (!hasMoreElements(resultSet)) {
       throw new RuntimeException("Couldn't find player " + firstName + " " + lastName);
     }
-    int playerID = getInt(resultSet, "ID");
-    if (hasMoreElements(resultSet)) {
-      throw new RuntimeException("Found multiple matches for player " + firstName + " " + lastName);
-    }
-    return playerID;
   }
 
-  private static void removeExistingKeepers() {
-    executeUpdate("DELETE FROM Keepers");
+  private void removeExistingKeepers() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate("DELETE FROM Keepers");
   }
 }
